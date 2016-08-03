9e5313d56f9ca444491a82a1036b3d5a215f1ea8
underdolphin <underdolphin@users.noreply.github.com>
2016-08-03 17:43:11 +0900

    fix typo comma to semicolon

--- felt/felt.d.ts
+++ felt/felt.d.ts
@@ -4,15 +4,15 @@ declare module 'felt' {
     function Felt(opts?: Felt.Opts);
     namespace Felt {
         interface Opts {
-            src: string,
-            cache?: string,
-            root?: Function,
-            handlers?: {},
-            patterns?: string | string[],
-            update?: string,
-            reflesh?: boolean,
-            watch?: boolean,
-            debug?: boolean
+            src: string;
+            cache?: string;
+            root?: Function;
+            handlers?: {};
+            patterns?: string | string[];
+            update?: string;
+            reflesh?: boolean;
+            watch?: boolean;
+            debug?: boolean;
         }
     }
     export = Felt;
