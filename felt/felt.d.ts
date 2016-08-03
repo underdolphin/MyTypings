4c1ac20d94987fc5a2778d9305e70eb342958588
underdolphin <underdolphin@users.noreply.github.com>
2016-08-03 17:46:32 +0900

    Reorder Felt functions

--- felt/felt.d.ts
+++ felt/felt.d.ts
@@ -1,7 +1,7 @@
 declare module 'felt' {
-    function Felt(recipe?: any, opts?: Felt.Opts);
-    function Felt(config: string, opts?: Felt.Opts);
     function Felt(opts?: Felt.Opts);
+    function Felt(recipe: any, opts?: Felt.Opts);
+    function Felt(config: string, opts?: Felt.Opts);
     namespace Felt {
         interface Opts {
             src: string;
